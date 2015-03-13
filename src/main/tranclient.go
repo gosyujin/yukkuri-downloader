2ac7ced330059b6d80d62571fb948453e7065885
kk_Ataka <ataka@example.com>
2015-03-13 17:37:21 +0900

    fmt source

--- src/main/tranclient.go
+++ src/main/tranclient.go
@@ -18,7 +18,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	
+
 	defer response.Body.Close()
 
 	fmt.Println(response.Header)
--- src/main/transerver.go
+++ src/main/transerver.go
@@ -1,25 +1,25 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 	"os"
 	"path/filepath"
-	"fmt"
-	"bytes"
-	"encoding/binary"
 )
 
 func main() {
 	m := martini.Classic()
-	
+
 	m.Use(render.Renderer(render.Options{
 		Directory: "templates",
-//		Layout: "layout",
+		//		Layout: "layout",
 		Extensions: []string{".tmpl"},
-		Charset: "utf-8",
+		Charset:    "utf-8",
 	}))
-	
+
 	m.Get("/", IndexRender)
 	m.Get("/dlFileList", dlFileList)
 	m.Run()
@@ -31,35 +31,34 @@ func dlFileList(params martini.Params) (int, string) {
 		if info.IsDir() {
 			return nil
 		}
-	
+
 		rel, err := filepath.Rel(root, path)
 		fmt.Println(rel)
-		
+
 		file, err := os.Open(path)
-		
+
 		if err != nil {
 			fmt.Println(1, err)
 		}
-		
+
 		b := make([]byte, 2)
 		file.Read(b)
-		
+
 		var val uint8
 		err2 := binary.Read(bytes.NewBuffer(b), binary.BigEndian, &val)
 
 		if err2 != nil {
 			fmt.Println(1, err)
 		}
-		
+
 		fmt.Println("readdata:", val)
 
 		return nil
 	})
-	
+
 	if err != nil {
 		fmt.Println(1, err)
 	}
-	
+
 	return 200, "End!"
 }
-
